26391428244d1af605ef57961889a21ea57ce17d
AllenJue <mrallenjue@yahoo.com>
2023-01-20 17:39:50 -0600

    Initial commit

---
+++ fixedSort.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Prints size of elements of an array */
+void printArray(int arr[], int size) {
+  for(int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+/* Swap values of two numbers */
+void swap(int *i, int *j) {
+  int temp = *i;
+  // i = NULL; Trying to dereference NULL is bad!
+  *i = *j;
+  *j = temp;
+}
+
+/* Do bubble sort. smaller values bubble to the front */
+void bubbleSort(int arr[], int size) {
+  for(int i = 0; i < size; i++) {
+    for(int j = 1; j < size; j++) { // OBOE
+      // if a number is smaller than its previous, bubble it to the front
+      if(arr[j] < arr[j - 1]) {
+        swap(&arr[j], &arr[j - 1]);
+      }
+    }
+  }
+}
+
+/* The Debugging Tutorial! */
+int main(void) {
+  // we have an array of 5 elements
+  int SIZE = 5;
+  int *arr = malloc(sizeof(int) * SIZE);
+  for(int i = 0; i < SIZE; i++) {
+    arr[i] = random() % 100; 
+  }
+  printf("Before sorting: \n");
+  printArray(arr, SIZE);
+
+  printf("After sorting: \n");
+  bubbleSort(arr, SIZE);
+  printArray(arr, SIZE);
+  return 0;
+}
+
+
+
+
